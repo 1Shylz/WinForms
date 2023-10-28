0248a0d7ffdc58a04d4ab812dc4b88d9ea08941f
Shylz <1_shylz@mail.ru>
2023-10-28 23:40:33 +1000

    добавил label и button

--- Forms/WinForm1/WinForm1/Form1.cs
+++ Forms/WinForm1/WinForm1/Form1.cs
@@ -11,9 +11,16 @@ namespace WinForm1
 {
     public partial class Form1 : Form
     {
+        int k = 0;
         public Form1()
         {
             InitializeComponent();
+            label1.Text= "hello world";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            k=k+1;
+            label1.Text = "pressed " + k + "  times";
         }
     }
 }
