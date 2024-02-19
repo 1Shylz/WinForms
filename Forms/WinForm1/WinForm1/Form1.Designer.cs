739eb0bec079d6ea5f7dca874a301b3f05beb4a6
Shylz <1_shylz@mail.ru>
2024-02-20 00:22:53 +1000

    я вернулся

--- Forms/WinForm1/WinForm1/Form1.Designer.cs
+++ Forms/WinForm1/WinForm1/Form1.Designer.cs
@@ -28,18 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_load = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button_save = new System.Windows.Forms.Button();
+            this.textBox_description = new System.Windows.Forms.TextBox();
+            this.textBox_responsible = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button_add = new System.Windows.Forms.Button();
+            this.textBox_status = new System.Windows.Forms.TextBox();
+            this.textBox_tag = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox6 = new System.Windows.Forms.TextBox();
@@ -54,18 +55,22 @@
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton_add = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // button_load
             // 
-            this.button1.Location = new System.Drawing.Point(1080, 212);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 45);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "загрузить";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button_load.Location = new System.Drawing.Point(1080, 212);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(112, 45);
+            this.button_load.TabIndex = 0;
+            this.button_load.Text = "загрузить";
+            this.button_load.UseVisualStyleBackColor = true;
+            this.button_load.Click += new System.EventHandler(this.button_load_Click);
             // 
             // dataGridView1
             // 
@@ -83,44 +88,39 @@
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 5;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
             this.dataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(1003, 360);
             this.dataGridView1.TabIndex = 4;
             // 
-            // button2
+            // button_save
             // 
-            this.button2.Location = new System.Drawing.Point(1080, 341);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 45);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "сохранить";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Visible = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button_save.Location = new System.Drawing.Point(1080, 341);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(112, 45);
+            this.button_save.TabIndex = 5;
+            this.button_save.Text = "сохранить";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Visible = false;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
             // 
-            // textBox1
+            // textBox_description
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 46);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(140, 20);
-            this.textBox1.TabIndex = 6;
+            this.textBox_description.Location = new System.Drawing.Point(206, 46);
+            this.textBox_description.Multiline = true;
+            this.textBox_description.Name = "textBox_description";
+            this.textBox_description.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_description.Size = new System.Drawing.Size(502, 74);
+            this.textBox_description.TabIndex = 7;
             // 
-            // textBox2
+            // textBox_responsible
             // 
-            this.textBox2.Location = new System.Drawing.Point(206, 46);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(502, 74);
-            this.textBox2.TabIndex = 7;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(787, 46);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(180, 20);
-            this.textBox3.TabIndex = 8;
+            this.textBox_responsible.Location = new System.Drawing.Point(787, 46);
+            this.textBox_responsible.Name = "textBox_responsible";
+            this.textBox_responsible.Size = new System.Drawing.Size(180, 20);
+            this.textBox_responsible.TabIndex = 8;
             // 
             // label1
             // 
@@ -149,30 +149,30 @@
             this.label3.TabIndex = 11;
             this.label3.Text = "ответственнй";
             // 
-            // button3
+            // button_add
             // 
-            this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(1009, 20);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 100);
-            this.button3.TabIndex = 12;
-            this.button3.Text = "добавить";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button_add.Enabled = false;
+            this.button_add.Location = new System.Drawing.Point(1009, 20);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(100, 100);
+            this.button_add.TabIndex = 12;
+            this.button_add.Text = "добавить";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
             // 
-            // textBox4
+            // textBox_status
             // 
-            this.textBox4.Location = new System.Drawing.Point(819, 100);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 13;
+            this.textBox_status.Location = new System.Drawing.Point(819, 100);
+            this.textBox_status.Name = "textBox_status";
+            this.textBox_status.Size = new System.Drawing.Size(100, 20);
+            this.textBox_status.TabIndex = 13;
             // 
-            // textBox5
+            // textBox_tag
             // 
-            this.textBox5.Location = new System.Drawing.Point(12, 100);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 14;
+            this.textBox_tag.Location = new System.Drawing.Point(12, 100);
+            this.textBox_tag.Name = "textBox_tag";
+            this.textBox_tag.Size = new System.Drawing.Size(100, 20);
+            this.textBox_tag.TabIndex = 14;
             // 
             // label4
             // 
@@ -286,17 +286,53 @@
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(12, 7);
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 46);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(140, 20);
             this.dateTimePicker1.TabIndex = 28;
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton_add,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(1264, 25);
+            this.toolStrip1.TabIndex = 29;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton_add
+            // 
+            this.toolStripButton_add.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.toolStripButton_add.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton_add.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_add.Image")));
+            this.toolStripButton_add.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripButton_add.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_add.Margin = new System.Windows.Forms.Padding(0, 1, 5, 2);
+            this.toolStripButton_add.Name = "toolStripButton_add";
+            this.toolStripButton_add.Size = new System.Drawing.Size(102, 22);
+            this.toolStripButton_add.Text = "Добавить задачу";
+            this.toolStripButton_add.Click += new System.EventHandler(this.toolStripButton_add_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "кнопка";
+            // 
             // Form1
             // 
-            this.AcceptButton = this.button1;
+            this.AcceptButton = this.button_load;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox10);
@@ -311,24 +347,25 @@
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox_tag);
+            this.Controls.Add(this.textBox_status);
+            this.Controls.Add(this.button_add);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox_responsible);
+            this.Controls.Add(this.textBox_description);
+            this.Controls.Add(this.button_save);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_load);
             this.MaximumSize = new System.Drawing.Size(1280, 720);
             this.MinimumSize = new System.Drawing.Size(1280, 720);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Планер";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -336,18 +373,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_load;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.TextBox textBox_description;
+        private System.Windows.Forms.TextBox textBox_responsible;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.TextBox textBox_status;
+        private System.Windows.Forms.TextBox textBox_tag;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox6;
@@ -362,6 +398,9 @@
         private System.Windows.Forms.TextBox textBox10;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton_add;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
     }
 }
 
